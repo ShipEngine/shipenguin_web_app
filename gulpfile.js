ad54725acd3168e80a2c917c19889a7d35639102
KaseyCantu-SE <kasey.cantu@shipengine.com>
2020-07-27 02:00:48 -0500

    Added JS files to the watch task as well as fixes to sourcemap.write().

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require("gulp"),
+let gulp = require("gulp"),
     sass = require("gulp-sass"),
     postcss = require("gulp-postcss"),
     autoprefixer = require("autoprefixer"),
@@ -6,7 +6,7 @@ var gulp = require("gulp"),
     sourcemaps = require("gulp-sourcemaps"),
     browserSync = require("browser-sync").create();
 
-var paths = {
+let paths = {
     styles: {
         src: "./public/styles/*.scss",
         dest: "public/css"
@@ -20,30 +20,27 @@ function style() {
         .pipe(sass())
         .on("error", sass.logError)
         .pipe(postcss([
-            autoprefixer(), 
+            autoprefixer(),
             cssnano()
         ]))
-        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write("."))
         .pipe(gulp.dest(paths.styles.dest))
         .pipe(browserSync.stream());
-}
-
-function reload() {
-    browserSync.reload();
 }
 
 function watch() {
     browserSync.init({
         notify: false,
-        proxy: ""
+        proxy: "shipenguin.ngrok.io"
     });
     gulp.watch("./public/styles/**/*.scss", style);
-    gulp.watch("public/*.html").on('change', browserSync.reload);
+    gulp.watch("./public/*.html").on("change", browserSync.reload);
+    gulp.watch("./public/js/**/*.js").on("change", browserSync.reload);
 }
 
-exports.watch = watch
+exports.watch = watch;
 exports.style = style;
 
-var build = gulp.parallel(style, watch);
+let build = gulp.parallel(style, watch);
 
 gulp.task('default', build);
